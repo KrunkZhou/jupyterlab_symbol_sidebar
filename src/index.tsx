023175ea5709e92b7721555ceb9ec55fa646d560
Krunk Zhou <26049401+KrunkZhou@users.noreply.github.com>
2023-03-10 11:26:16 -0500

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -6,26 +6,59 @@ import {
 
 import * as React from 'react';
 import { ReactWidget } from '@jupyterlab/apputils';
-
 import {codeList} from './code';
 
-/**
- * Initialization data for the jupyterlab-sidepanel extension.
- */
 const extension: JupyterFrontEndPlugin<void> = {
-  id: 'TESTEX:plugin',
+  id: 'jupyterlab_symbol_sidebar:plugin',
   autoStart: true,
   requires: [ILabShell],
   activate: (app: JupyterFrontEnd, shell: ILabShell) => {
-    console.log('JupyterLab extension TESTEX is activated!');
+    console.log('JupyterLab extension jupyterlab_symbol_sidebar is activated!');
+
+    var checkboxCopy = false;
+
+    function SymbolsSidebar() {
+      var list = codeList;
+      return (
+        <div className="sidebar-container">
+          <div className="notice">Click on the icon will insert it to the cell</div>
+          <label className="checkbox">
+            <input
+              type="checkbox"
+              onChange={() => {
+                  checkboxCopy = !checkboxCopy;
+                }}
+            />
+            Copy to clipboard
+          </label>
+              <div className="block_container">
+                {list.map((item,i) => 
+                  <div 
+                    className="block_container_element"
+                    key={i} 
+                    onClick={() => iconClick(item.unicode)}
+                  >
+                    {item.unicode}
+                    <div>{item.name}</div>
+                  </div>)}
+              </div>
+        </div>
+        );
+    }
+
+    function iconClick(code:any) {
+      if (checkboxCopy)
+        navigator.clipboard.writeText(JSON.parse(`["`+code+`"]`)[0].toString())
+      else
+        app.commands.execute('unicode:insert', { text: JSON.parse(`["`+code+`"]`)[0].toString() });                 
+    }
 
     const newWidget = () => {
       // Create a blank content widget inside of a MainAreaWidget
       const widget = ReactWidget.create(
-        <MyComponent />
+        <SymbolsSidebar />
       );
-      //const widget = new MainAreaWidget({ content });
-      widget.id = 'symbols-jupyterlab';
+      widget.id = 'jupyterlab_symbol_sidebar';
       widget.title.label = 'Symbols';
       widget.title.closable = true;
       return widget;
@@ -39,31 +72,5 @@ const extension: JupyterFrontEndPlugin<void> = {
     shell.add(widget, 'left');
   }
 };
-
-function MyComponent() {
-  var list = codeList;
-
-  return (
-    <div className="sidebar-container">
-      <div className="notice">Click on icon will copy to clipboard</div>
-          <div className="block_container">
-            {list.map((item,i) => 
-              <div 
-                key={i} 
-                onClick={() => iconClick(item.unicode)}
-              >
-                {item.unicode}
-                <div>{item.name}</div>
-              </div>)}
-          </div>
-    </div>
-    );
-}
-
-function iconClick(code:any) {
-  //JSON.parse(`["${item.unicode}"]`)[0].toString()
-  navigator.clipboard.writeText(JSON.parse(`["`+code+`"]`)[0].toString())
-                   
-}
 
 export default extension;
