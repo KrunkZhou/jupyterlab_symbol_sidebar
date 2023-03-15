5b2474129eea368366fee793e7021e534e57d3ed
Krunk Zhou <26049401+KrunkZhou@users.noreply.github.com>
2023-03-14 22:06:53 -0400

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -6,64 +6,91 @@ import {
 
 import * as React from 'react';
 import { ReactWidget } from '@jupyterlab/apputils';
-import {codeList} from './code';
+import { codeList } from './code';
+import { INotebookTools } from '@jupyterlab/notebook';
 
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_symbol_sidebar:plugin',
   autoStart: true,
-  requires: [ILabShell],
-  activate: (app: JupyterFrontEnd, shell: ILabShell) => {
+  requires: [INotebookTools, ILabShell],
+  activate: (
+    app: JupyterFrontEnd,
+    notebook: INotebookTools,
+    shell: ILabShell
+  ) => {
     console.log('JupyterLab extension jupyterlab_symbol_sidebar is activated!');
 
-    var checkboxCopy = false;
+    let checkboxCopy = false;
 
     function SymbolsSidebar() {
-      var list = codeList;
+      const list = codeList;
       return (
         <div className="sidebar-container">
-          <div className="notice">Click on the icon will insert it to the cell</div>
+          <div className="notice">
+            Click on the icon will insert it to the cell
+          </div>
           <label className="checkbox">
             <input
               type="checkbox"
               onChange={() => {
-                  checkboxCopy = !checkboxCopy;
-                }}
+                checkboxCopy = !checkboxCopy;
+              }}
             />
             Copy to clipboard
           </label>
-              <div className="block_container">
-                {list.map((item,i) => 
-                  <div 
-                    className="block_container_element"
-                    key={i} 
-                    onClick={() => iconClick(item.unicode)}
-                  >
-                    {item.unicode}
-                    <div>{item.name}</div>
-                  </div>)}
+          <div className="block-container">
+            {list.map((item, i) => (
+              <div
+                className="block-container-element"
+                key={i}
+                onClick={() => iconClick(item.unicode)}
+              >
+                {item.unicode}
+                <div>{item.name}</div>
               </div>
+            ))}
+          </div>
         </div>
-        );
+      );
     }
 
-    function iconClick(code:any) {
-      if (checkboxCopy)
-        navigator.clipboard.writeText(JSON.parse(`["`+code+`"]`)[0].toString())
-      else
-        app.commands.execute('unicode:insert', { text: JSON.parse(`["`+code+`"]`)[0].toString() });                 
+    app.commands.addCommand('unicode:insert', {
+      label: 'Insert Text',
+      // isVisible: () => false,
+      execute: args => {
+        // input is JSON
+        if (typeof args.text !== 'string') {
+          return;
+        } // if no text, return
+        if (notebook.activeCell === null) {
+          return;
+        }
+        notebook.activeCell.editor.replaceSelection &&
+          notebook.activeCell.editor.replaceSelection(args.text.toString());
+      }
+    });
+
+    function iconClick(code: any) {
+      if (checkboxCopy) {
+        navigator.clipboard.writeText(
+          JSON.parse('["' + code + '"]')[0].toString()
+        );
+      } else {
+        app.commands.execute('unicode:insert', {
+          text: JSON.parse('["' + code + '"]')[0].toString()
+        });
+      }
     }
 
     const newWidget = () => {
       // Create a blank content widget inside of a MainAreaWidget
-      const widget = ReactWidget.create(
-        <SymbolsSidebar />
-      );
+      const widget = ReactWidget.create(<SymbolsSidebar />);
       widget.id = 'jupyterlab_symbol_sidebar';
       widget.title.label = 'Symbols';
       widget.title.closable = true;
       return widget;
-    }
-    let widget = newWidget();
+    };
+    const widget = newWidget();
 
     // let summary = document.createElement('p');
     // widget.node.appendChild(summary);
--- ui-tests/tests/jupyterlab_symbol_sidebar.spec.ts
+++ ui-tests/tests/jupyterlab_symbol_sidebar.spec.ts
@@ -16,6 +16,8 @@ test('should emit an activation console message', async ({ page }) => {
   await page.goto();
 
   expect(
-    logs.filter(s => s === 'JupyterLab extension jupyterlab-symbol-sidebar is activated!')
+    logs.filter(
+      s => s === 'JupyterLab extension jupyterlab-symbol-sidebar is activated!'
+    )
   ).toHaveLength(1);
 });
